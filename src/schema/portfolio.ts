cf25b05d64c4e68708352ba9cae4ee8d19d93a89
muhammadtayyabasghar@gmail.com <M Tayyab Asghar>
2026-04-12 05:54:12 +0500

    👌 IMPROVE: Updated the personal projects sections.

--- src/schema/portfolio.ts
+++ src/schema/portfolio.ts
@@ -2,15 +2,15 @@ import { z } from "astro:content";
 
 const portfolioSchema = z.object({
   title: z.string(),
-  description: z.string(),
-  bgGradient:z.string(),
+  image: z.string(),
   priority:z.number(),  //TODO: Remove after dividing the projects into side projects.
+  bgGradient:z.string(),
+  description: z.string(),
   author: z.string().trim(),
   company: z.string().trim(),
   draft: z.boolean().optional(),
   pinned: z.boolean().optional(),
   tags: z.array(z.string()),
-  image: z.string().optional(),
   publishDate: z.string().transform((str) =>
     new Date(str).toLocaleDateString("en-GB", {
       year: "numeric",
