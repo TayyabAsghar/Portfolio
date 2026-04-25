e2f85124f10dabab3854247f11a515a3ae070e3a
muhammadtayyabasghar@gmail.com <M Tayyab Asghar>
2026-04-25 21:54:55 +0500

    👌 IMPROVE: Updated the protfolio projects design and added some more svg.

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,6 +1,7 @@
 export { default as Stack } from "./stack.astro";
 export { default as ListPosts } from "./list-posts.astro";
 export { default as ImageTile } from "./image-tile.astro";
+export { default as ImpactCards } from "./impact-cards.astro";
 export { default as GroupedPosts } from "./grouped-posts.astro";
 export { default as LightBoxImage } from "./light-box-image.astro";
 export { default as PlanetaryOrbit } from "./planetary-orbit.astro";
